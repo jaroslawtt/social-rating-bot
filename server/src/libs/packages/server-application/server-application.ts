99ef9b656c61a09e8b670a4290b4e9d5a321475c
jaroslawtt <yaroslavlogan61907@gmail.com>
2023-10-17 22:14:36 +0300

    Implemented backend architecture

---
+++ server/src/libs/packages/server-application/server-application.ts
@@ -0,0 +1,20 @@
+import { config } from '~/libs/packages/config/config.js';
+import { database } from '~/libs/packages/database/database.js';
+import { logger } from '~/libs/packages/logger/logger.js';
+
+import { ServerApp } from './server-app.js';
+import { ServerAppApi } from './server-app-api.js';
+
+const apiV1 = new ServerAppApi(
+  'v1',
+  config,
+);
+const serverApp = new ServerApp({
+  config,
+  logger,
+  database,
+  apis: [apiV1],
+});
+
+export { serverApp };
+export { type ServerAppRouteParameters } from './libs/types/types.js';
